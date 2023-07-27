b596bfdbf52b2c0de332d6eba438b3826b63b7c1
MI KE <fernandesmandrew@gmail.com>
2023-07-27 12:07:48 +0800

    Start Client Server learning

---
+++ server.js
@@ -0,0 +1,6 @@
+// SERVER listens for events (requests) from a browser
+// Server contains the website and listens for requests for that website
+// It can be an image, audio, and HTML files
+
+// GET  - receive a resource from the server
+// POST - send a data to the server
