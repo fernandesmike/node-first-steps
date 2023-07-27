a030f1fa8cfe4a40683909ee74f00bd9307c1fca
MI KE <fernandesmandrew@gmail.com>
2023-07-27 15:02:51 +0800

    Added package dependencies and nodemon package

--- web-server.js
+++ web-server.js
@@ -2,14 +2,39 @@
 const http = require("http");
 const fs = require("fs");
 
-const WEB_PATH = "./files/index.html";
+const FILE_PATH = "./files/";
 
 const server = http.createServer((req, res) => {
   // Always set the header of the response
   res.setHeader("Content-type", "text/html");
 
+  let path = "";
+
+  switch (req.url) {
+    case "/":
+      path = FILE_PATH.concat("index.html");
+      break;
+
+    case "/login":
+      path = FILE_PATH.concat("login.html");
+      break;
+
+    // This is a redirection process
+    case "/redirect":
+      // Status code for redirection
+      res.statusCode = 301;
+      // Set the redirection site
+      res.setHeader("Location", "/login");
+      res.end();
+      break;
+
+    default:
+      path = FILE_PATH.concat("404.html");
+      break;
+  }
+
   // Read the file
-  fs.readFile(WEB_PATH, (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err.message);
       res.end();
